8de9bb2383910f0ae32ef0cd95360b455e295524
yuran <yuranoshik@mail.ru>
2023-02-13 01:47:40 +0300

    Required

--- WpfAppVetclinic/Auth.xaml.cs
+++ WpfAppVetclinic/Auth.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EasyCaptcha.Wpf;
 
 namespace WpfAppVetclinic
 {
@@ -24,12 +25,27 @@ namespace WpfAppVetclinic
         public Auth()
         {
             InitializeComponent();
-            
+
         }
-        
+
+
+        string capchaLow;
+        string capchaUp;
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Captcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, 6);
+
+            capchaLow = Captcha.CaptchaText;
+            capchaUp = Captcha.CaptchaText;
+
+        }
         private void buttonReCaptcha_Click(object sender, RoutedEventArgs e)
         {
+            Captcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, 6);
 
+            capchaLow = Captcha.CaptchaText;
+            capchaUp = Captcha.CaptchaText;
         }
 
         private void buttonExit_Click(object sender, RoutedEventArgs e)
@@ -41,43 +57,62 @@ namespace WpfAppVetclinic
         {
             using (DostavkaEntities usersEntities = new DostavkaEntities())
             {
-                var customer = usersEntities.Stuffs.Where(x => x.stuffLogin == textBoxLogin.Text && x.stuffPassword == textBoxPassword.Text).First();
-                if (customer == null)
+                if (textBoxEnterCaptcha.Text == null || textBoxLogin.Text == null || textBoxPassword.Text == null)
                 {
-                    MessageBox.Show("Пользователь не найден");
+
+                    MessageBox.Show("Одно из полей пустое");
+
                 }
-
-
-                var roles = usersEntities.Roles.Where(x => x.RoleID == customer.stuffRole).First();
-                if (roles == null)
+                else
                 {
-                    MessageBox.Show("Роли не существует");
+                    try
+                    {
+                        var customer = usersEntities.Stuffs.Where(x => x.stuffLogin == textBoxLogin.Text && x.stuffPassword == textBoxPassword.Text).First();
+                        var roles = usersEntities.Roles.Where(x => x.RoleID == customer.stuffRole).First();
+
+
+                        if (customer == null || textBoxEnterCaptcha.Text.ToLowerInvariant() != capchaLow.ToLower() 
+                            || textBoxEnterCaptcha.Text.ToUpperInvariant() != capchaUp || textBoxLogin.Text != customer.stuffLogin || textBoxPassword.Text != customer.stuffPassword)
+                        {
+                            MessageBox.Show("Пользователь не найден");
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Здравствуйте, {customer.stuffName}, вы вошли в систему, как {roles.RoleName}");
+
+                            switch (roles.RoleID)
+                            {
+                                case 1:
+                                    new AdminForm().Show();
+                                    break;
+                                case 2:
+                                    new Katalog().Show();
+                                    break;
+                                case 3:
+                                    new ManagerForm().Show();
+                                    break;
+                            }
+
+                        }
+
+                    }
+                    catch(InvalidOperationException)
+                    {
+                        MessageBox.Show("Неверно введены данные");
+                    }
+
+
                 }
-
-                MessageBox.Show($"Дарова, {customer.stuffName} ты {roles.RoleName}");
-
-
-                
-                switch(roles.RoleID)
-                {
-                    case 1:
-                        new AdminForm().Show();
-                        break;
-                    case 2:
-                        new Katalog().Show();
-                        break;
-                    case 3:
-                        new ManagerForm().Show();
-                        break;
-                }
-                
             }
-            
-            //Captcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, 6);
-            //var answer = Captcha.CaptchaText;
-            //return answer;
+            textBoxLogin.Text = "";
+            textBoxPassword.Text = "";
+            textBoxEnterCaptcha.Text = "";
+            Captcha.CreateCaptcha(Captcha.LetterOption.Alphanumeric, 6);
         }
-        
+
     }
-}
+
+
+}    
+
 
